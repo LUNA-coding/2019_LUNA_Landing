b5442381572bbffee5fd2cb66b600a5d34ad4da3
kdwkr <me@kdw.kr>
2019-01-29 02:22:42 +0900

    이쁘게 세팅

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <section id="Main" class="section">
-    <img class="bg" src="@/assets/img/bg_main.png">
+    <img class="bg" ref="bg" src="@/assets/img/bg_main.png">
+    <div class="bg2" :style="{'top': `calc(50% + ${bgHeight/2}px)`}"></div>
     <div class="bottom">
       <div class="social-list">
         <a class="social-item" href="https://fb.com/lunacoding" target="_blank"><img src="@/assets/img/ic_facebook.png"></a>
@@ -16,6 +17,14 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Main extends Vue {
+  private bgHeight = 0;
+  private mounted() {
+    const setBgHeight = () => {
+      this.bgHeight = this.$refs.bg.clientHeight - 2;
+      setTimeout(setBgHeight,100);
+    };
+    this.$nextTick(setBgHeight);
+  }
 }
 </script>
 
@@ -33,6 +42,14 @@ body.main {
   bottom: 0;
   width: 100vw;
   z-index: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.bg2 {
+  position: absolute;
+  bottom: 0;
+  background: #060E29;
+  width: 100%;
 }
 .bottom {
   position: absolute;
