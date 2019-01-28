0a6637cd5090392e607282a8765328c0f03a2c6c
kdwkr <me@kdw.kr>
2019-01-29 01:56:02 +0900

    헤더 항목 수정

--- src/components/luna/Header.vue
+++ src/components/luna/Header.vue
@@ -1,11 +1,14 @@
 <template>
   <header class="__luna_header">
     <LUNA-NavList>
-      <LUNA-NavItem link="/">MAIN</LUNA-NavItem>
-      <LUNA-NavItem link="/2019">2019 LUNA</LUNA-NavItem>
-      <LUNA-NavItem link="/2018">2018 LUNA</LUNA-NavItem>
-      <LUNA-NavItem link="/member">MEMBER</LUNA-NavItem>
-      <LUNA-NavItem link="/achievement">ACHIEVEMENT</LUNA-NavItem>
+      <LUNA-NavItem link="/luna">LUNA</LUNA-NavItem>
+      <LUNA-NavItem link="/2018">2018</LUNA-NavItem>
+      <LUNA-NavItem link="/2019">2019</LUNA-NavItem>
+      <LUNA-NavItem link="/achievement">Achievement</LUNA-NavItem>
+      <LUNA-NavItem link="/" type="image"><img src="@/assets/img/logo.png"></LUNA-NavItem>
+      <LUNA-NavItem link="/member">Member</LUNA-NavItem>
+      <LUNA-NavItem link="/cooperation">Cooperation</LUNA-NavItem>
+      <LUNA-NavItem link="/galaxy">Galaxy</LUNA-NavItem>
       <LUNA-NavItem link="/faq">FAQ</LUNA-NavItem>
     </LUNA-NavList>
   </header>
