b4fff6780ca2cc71da6bc82b72d094dc89d86b70
kdwkr <me@kdw.kr>
2019-01-29 01:55:05 +0900

    vue config 수정

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  productionSourceMap: false,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  parallel: undefined,
+  css: undefined
+}
